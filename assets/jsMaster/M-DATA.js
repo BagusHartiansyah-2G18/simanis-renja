8965bc56da0c849d6baff56ba63ef5d0345b94ee
bagushartiansyah <bagushartiansyah07@gmail.com>
2025-11-03 06:54:35 +0700

    (fix) feature renstra, persentase

--- assets/jsMaster/M-DATA.js
+++ assets/jsMaster/M-DATA.js
@@ -137,7 +137,7 @@ function informasix() {
 }
 
 function newInfo(persen) {  
-    const all = persen[0].persentse;
+    const all = ((_.totalPagu/_.totalRealisasi)*100).toFixed(2);
     const allSisa = 100-all;
     let fhtml ='';
     fhtml=`
